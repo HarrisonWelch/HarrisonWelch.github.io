54b3a56c8309e506fdb9929491df06b369b2ed45
HarrisonWelch <hwelch1@my.apsu.edu>
2020-09-11 01:22:17 -0500

    Contact Us now works by auto-filling in an email for the user.

--- assets/js/harrison.js
+++ assets/js/harrison.js
@@ -1,3 +1,18 @@
+// Contact Us function
 document.getElementById("sendMessageButton").onclick = function sendMessage() {
   console.log('sendMessage');
+
+  var name = document.getElementById("sendMessageName").value;
+  console.log('name = ' + name);
+
+  var email = document.getElementById("sendMessageEmail").value;
+  console.log('email = ' + email);
+  
+  var body = document.getElementById("sendMessageBody").value;
+  console.log('body = ' + body);
+
+  subject = 'Github Pages | From: ' + name;
+  body = body + ' ' + 'Please respond to ' + email;
+
+  window.open('mailto:harrison.welch@gmail.com?subject=' + subject + '&body=' + body);
 }
