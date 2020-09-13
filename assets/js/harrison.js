7b53590e79e3d2c0213dd49cdf5f9538f0819cca
HarrisonWelch <hwelch1@my.apsu.edu>
2020-09-12 22:24:32 -0500

    Added support for Formspree for the contact us section

--- assets/js/harrison.js
+++ assets/js/harrison.js
@@ -1,18 +1,18 @@
-// Contact Us function
-document.getElementById("sendMessageButton").onclick = function sendMessage() {
-  console.log('sendMessage');
+// // Contact Us function
+// document.getElementById("sendMessageButton").onclick = function sendMessage() {
+//   console.log('sendMessage');
 
-  var name = document.getElementById("sendMessageName").value;
-  // console.log('name = ' + name);
+//   var name = document.getElementById("sendMessageName").value;
+//   // console.log('name = ' + name);
 
-  var email = document.getElementById("sendMessageEmail").value;
-  // console.log('email = ' + email);
+//   var email = document.getElementById("sendMessageEmail").value;
+//   // console.log('email = ' + email);
   
-  var body = document.getElementById("sendMessageBody").value;
-  // console.log('body = ' + body);
+//   var body = document.getElementById("sendMessageBody").value;
+//   // console.log('body = ' + body);
 
-  subject = 'Github Pages | From: ' + name;
-  body = body + ' ' + 'Please respond to ' + email;
+//   subject = 'Github Pages | From: ' + name;
+//   body = body + ' ' + 'Please respond to ' + email;
 
-  window.open('mailto:harrison.welch@gmail.com?subject=' + subject + '&body=' + body);
-}
+//   window.open('mailto:harrison.welch@gmail.com?subject=' + subject + '&body=' + body);
+// }
